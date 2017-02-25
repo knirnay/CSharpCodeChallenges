723952a68da59470ba9e77648a7e235e79a1afcd
Nirnay <knirnay@gmail.com>
2017-02-25 15:30:11 -0800

    Adding some comments

--- CSharpCodeChallenges/BinaryTreeSearch/BinaryTreeSearcher.cs
+++ CSharpCodeChallenges/BinaryTreeSearch/BinaryTreeSearcher.cs
@@ -1,4 +1,10 @@
-﻿namespace CSharpCodeChallenges.BinaryTreeSearch
+﻿/// <summary>
+/// Binary search Tree example. 
+/// Binary search tree (BST) is a binary tree where the value of each node is larger or equal 
+/// to the values in all the nodes in that node's left subtree and is smaller than the values 
+/// in all the nodes in that node's right subtree.
+/// </summary>
+namespace CSharpCodeChallenges.BinaryTreeSearch
 {
     using System;
 
--- CSharpCodeChallenges/Palindrome.cs
+++ CSharpCodeChallenges/Palindrome.cs
@@ -1,4 +1,11 @@
-﻿namespace CSharpCodeChallenges
+﻿/// <summary>
+/// A palindrome is a word that reads the same backward or forward. 
+/// Write a function that checks if a given word is a palindrome. 
+/// Character case should be ignored. For example, IsPalindrome("Deleveled") should return 
+/// true as character case should be ignored, resulting in "deleveled", which is a palindrome 
+/// since it reads the same backward and forward.
+/// </summary>
+namespace CSharpCodeChallenges
 {
     using System;
     using System.Collections.Generic;
